9c70a9c33b754524a87422e07e05505ecb93260d
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-06-25 10:20:26 -0300

    remove: prop drilling from app.jsx add: function adicionar produto

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
+import { CarrinhoContext } from "@/context/CarrinhoContext";
 
-const Produtos = ({ adicionarProduto }) => {
+const Produtos = () => {
+  //criamos uma variável com chaves pra indicar que vamos desestruturar algo, nesse caso o hook useContext, que iremos usar somente o carrinho e o setCarrinho
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+  function adicionarProduto(novoProduto) {
+    //verifica se tem o produto e percorre o carrinho com a função some do javascript pra verificar se tem algum elemento dentro do carrinho que retorna true ou false
+    const temOProduto = carrinho.some((itemDoCarrinho) => {
+      //pra cada item do carrinho, verifica se o id é o mesmo do produto a adicionar
+      itemDoCarrinho.id === novoProduto.id;
+    });
+    //se não tiver o produto, adiciona no carrinho
+    if (!temOProduto) {
+      novoProduto.quantidade = 1;
+      //usa o carrinho anterior como o estado e adiciona o novo produto nesse estado
+      return setCarrinho((carrinhoAnterior) => [
+        ...carrinhoAnterior,
+        novoProduto,
+      ]);
+    }
+    //se o produto já estiver no carrinho, aumenta a quantidade dele
+    setCarrinho((carrinhoAnterior) =>
+      carrinhoAnterior.map((itemDoCarrinho) => {
+        if (itemDoCarrinho.id === novoProduto.id)
+          itemDoCarrinho.quantidade += 1;
+        return itemDoCarrinho;
+      })
+    );
+  }
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,14 +1,16 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 //usamos a função do createContext que retorna um objeto de contexto
 export const CarrinhoContext = createContext();
 
 //Esse objeto de contexto é então usado para criar um componente Provider, que compartilha o contexto com os componentes filhos.
-export const CarrinhoProvider = ({children}) => {
-    return (
-        //O Provider é responsável por envolver os componentes que precisam acessar os dados do contexto.
-        <CarrinhoContext.Provider>
-            {children}
-        </CarrinhoContext.Provider>
-    )
-}
+export const CarrinhoProvider = ({ children }) => {
+  const [carrinho, setCarrinho] = useState([]);
+
+  return (
+    //O Provider é responsável por envolver os componentes que precisam acessar os dados do contexto.
+    <CarrinhoContext.Provider value={{ carrinho, setCarrinho }}>
+      {children}
+    </CarrinhoContext.Provider>
+  );
+};
