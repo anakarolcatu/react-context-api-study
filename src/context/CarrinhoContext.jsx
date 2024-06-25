c97ff5aaf362513fb4f326857f69500146fad48e
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-06-25 18:02:30 -0300

    add: reducer for the shopping card

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useState } from "react";
+import { createContext, useEffect, useMemo, useReducer, useState } from "react";
 import { carrinhoReducer } from "../reducers/carrinhoReducer";
 
 //usamos a função do createContext que retorna um objeto de contexto
@@ -12,6 +12,25 @@ export const CarrinhoProvider = ({ children }) => {
   const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial);
   const [quantidade, setQuantidade] = useState(0);
   const [valorTotal, setValorTotal] = useState(0);
+
+  const { totalTemp, quantidadeTemp } = useMemo(() => {
+    return carrinho.reduce(
+      (acumulador, produto) => ({
+        quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+        totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
+      }), 
+      {
+        quantidadeTemp: 0,
+        totalTemp: 0,
+      }
+    );
+  }, [carrinho]);
+    
+  //usa o hook useEffect pra monitorar o carrinho e exibir a quantidade e preço atualizado de acordo com os produtos no carrinho
+  useEffect(() => {
+    setQuantidade(quantidadeTemp);
+    setValorTotal(totalTemp);
+  });
 
   return (
     //O Provider é responsável por envolver os componentes que precisam acessar os dados do contexto.
--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -1,91 +1,53 @@
-import { useContext, useEffect, useMemo } from "react";
+import { useContext } from "react";
 import { CarrinhoContext } from "@/context/CarrinhoContext";
+import { ADD_PRODUTO, REMOVE_PRODUTO, UPDATE_QUANTIDADE } from "../reducers/carrinhoReducer";
+
+const addProdutoAction = (novoProduto) => ({
+  type: ADD_PRODUTO,
+  payload: novoProduto,
+});
+
+const removeProdutoAction = (produtoId) => ({
+  type: REMOVE_PRODUTO,
+  payload: produtoId,
+});
+
+const updateQuantidadeAction = (produtoId, quantidade) => ({
+  type: UPDATE_QUANTIDADE,
+  payload: {produtoId, quantidade},
+});
 
 //hook customizado para adicionar a remover produtos
 export const useCarrinhoContext = () => {
   //criamos uma variável com chaves pra indicar que vamos desestruturar algo, nesse caso o hook useContext, que iremos usar somente o carrinho e o setCarrinho
   const {
     carrinho,
-    setCarrinho,
+    dispatch,
     quantidade,
-    setQuantidade,
     valorTotal,
-    setValorTotal,
   } = useContext(CarrinhoContext);
 
-  function mudarQuantidade(id, quantidade) {
-    return carrinho.map((itemDoCarrinho) => {
-      if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade += quantidade;
-      return itemDoCarrinho;
-    });
-  }
-
   function adicionarProduto(novoProduto) {
-    //verifica se tem o produto e percorre o carrinho com a função some do javascript pra verificar se tem algum elemento dentro do carrinho que retorna true ou false
-    const temOProduto = carrinho.some(
-      (itemDoCarrinho) =>
-        //pra cada item do carrinho, verifica se o id é o mesmo do produto a adicionar
-        itemDoCarrinho.id === novoProduto.id
-    );
-    //se não tiver o produto, adiciona no carrinho
-    if (!temOProduto) {
-      novoProduto.quantidade = 1;
-      //usa o carrinho anterior como o estado e adiciona o novo produto nesse estado
-      return setCarrinho((carrinhoAnterior) => [
-        ...carrinhoAnterior,
-        novoProduto,
-      ]);
-    }
-
-    const carrinhoAtualizado = mudarQuantidade(novoProduto.id, 1);
-    setCarrinho([...carrinhoAtualizado]);
+    dispatch(addProdutoAction(novoProduto));
   }
 
   function removerProduto(id) {
     //tenta encontrar o produto
-    const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
-    //verifica se é o ultimo
-    const ultimo = produto.quantidade === 1;
-    //se for o ultimo, atualiza o estado do carrinho
-    if (ultimo) {
-      return setCarrinho((carrinhoAnterior) =>
-        carrinhoAnterior.filter((itemDoCarrinho) => itemDoCarrinho.id !== id)
-      );
+    const produto = carrinho.find((item) => item.id === id);
+    
+    if (produto && produto.quantidade > 1){
+      dispatch(updateQuantidadeAction(id, produto.quantidade -1));
+    } else {
+      dispatch(removeProdutoAction(id))
     }
-    const carrinhoAtualizado = mudarQuantidade(id, -1);
-    //se não for o ultimo, remove um item
-    setCarrinho([...carrinhoAtualizado]);
   }
 
   function removerProdutoCarrinho(id) {
-    //recebe o id do produto a ser removido e retorna uma lista de produtos que são diferentes do produto que será removido, então atualiza o estado do carrinho sem aquele produto
-    const produto = carrinho.filter(
-      (itemDoCarrinho) => itemDoCarrinho.id !== id
-    );
-    setCarrinho(produto);
+    dispatch(removeProdutoAction(id));
   }
-  const { totalTemp, quantidadeTemp } = useMemo(() => {
-    return carrinho.reduce(
-      (acumulador, produto) => ({
-        quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
-        totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
-      }), 
-      {
-        quantidadeTemp: 0,
-        totalTemp: 0,
-      }
-    );
-  }, [carrinho]);
-    
-  //usa o hook useEffect pra monitorar o carrinho e exibir a quantidade e preço atualizado de acordo com os produtos no carrinho
-  useEffect(() => {
-    setQuantidade(quantidadeTemp);
-    setValorTotal(totalTemp);
-  });
 
   return {
     carrinho,
-    setCarrinho,
     adicionarProduto,
     removerProduto,
     removerProdutoCarrinho,
